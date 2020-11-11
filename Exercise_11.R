a7f88f440b67840e2948e64257d1c5584e3b0f7e
Samantha Masca <samanthamasca@Samanthas-MacBook-Air.local>
2020-11-10 23:38:07 -0500

    Third exercise commit

--- Exercise_11.R
+++ Exercise_11.R
@@ -12,33 +12,29 @@ rMD <- 0.05 # cancer drug present, mutant
 
 times <- 1:500
 
-Ns <- matrix(data=NA, nrow=length(times), ncol=2)
+Ns <- matrix(data=NA, nrow=length(times), ncol=3)
 Ns[,1] <- times
 Ns[1,2] <- 2
-
-Ms <- matrix(data=NA, nrow=length(times), ncol=2)
-Ms[,1] <- times 
-Ms[1,2] <- 2
-
+Ns[1,3] <- 2
 
 
 # growth rate with mutation at 100 cells
 for(i in times[-1]){
-  if(Ns[,2] < 100) {
-    Ns[i,2] <- Ns[(i-1),2]+rN*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ms[(i-1),2])/K)
-    Ms[i,2] <- Ms[(i-1),2]+rM*Ms[(i-1),2]*(1-(Ns[(i-1),2]+Ms[(i-1),2])/K) 
+  if(Ns < 100) {
+    Ns[i,2] <- Ns[(i-1),2]+rN*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
+    Ns[i,3] <- Ns[(i-1),3]+rM*Ns[(i-1),3]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K) 
   }
   else {
-    Ns[i,2] <- Ns[(i-1),2]+rND*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ms[(i-1),2])/K)
-    Ms[i,2] <- Ms[(i-1),2]+rMD*Ms[(i-1),2]*(1-(Ns[(i-1),2]+Ms[(i-1),2])/K)
+    Ns[i,2] <- Ns[(i-1),2]+rND*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
+    Ns[i,3] <- Ns[(i-1),3]+rMD*Ns[(i-1),3]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
   }
 }
 
 
 # plotting results
-df <- data.frame(time=Ns[,1],pop=Ns[,2])
+df <- data.frame(time=Ns[,1], pop1=Ns[,2], pop2=Ns[,3])
 
-ggplot(data=df, aes(x=time, y=pop)) +
+ggplot(data=df, aes(x=time, y=pop1)) +
   geom_point() +
   theme_classic()
 
