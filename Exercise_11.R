2ebec4aea6926d1af958ca6c18fa504029672c58
Samantha Masca <samanthamasca@Samanthas-MacBook-Air.local>
2020-11-11 00:08:34 -0500

    Final exercise commit

--- Exercise_11.R
+++ Exercise_11.R
@@ -10,21 +10,21 @@ K <- 1000000 # carrying capacity
 rND <- -0.1 # cancer drug present, non-mutant 
 rMD <- 0.05 # cancer drug present, mutant 
 
-times <- 1:500
+times <- 1:500 # set timesteps
 
-Ns <- matrix(data=NA, nrow=length(times), ncol=3)
+Ns <- matrix(data=NA, nrow=length(times), ncol=3) # set matrix to hold updated population size
 Ns[,1] <- times
-Ns[1,2] <- 2
-Ns[1,3] <- 2
+Ns[1,2] <- 100
+Ns[1,3] <- 1
 
 
-# growth rate with mutation at 100 cells
+# growth rate of both populations, with mutation at 100 cells
 for(i in times[-1]){
-  if(Ns < 100) {
+  if(i < 150) {
     Ns[i,2] <- Ns[(i-1),2]+rN*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
     Ns[i,3] <- Ns[(i-1),3]+rM*Ns[(i-1),3]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K) 
   }
-  else {
+  else if(i > 149) {
     Ns[i,2] <- Ns[(i-1),2]+rND*Ns[(i-1),2]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
     Ns[i,3] <- Ns[(i-1),3]+rMD*Ns[(i-1),3]*(1-(Ns[(i-1),2]+Ns[(i-1),3])/K)
   }
@@ -32,9 +32,13 @@ for(i in times[-1]){
 
 
 # plotting results
-df <- data.frame(time=Ns[,1], pop1=Ns[,2], pop2=Ns[,3])
+df1 <- data.frame(time=Ns[,1], pop1=Ns[,2]) # non-mutant population
+df2 <- data.frame(time=Ns[,1],pop2=Ns[,3]) # mutant population
 
-ggplot(data=df, aes(x=time, y=pop1)) +
-  geom_point() +
+ggplot() +
+  geom_line(data=df1, aes(x=time, y=pop1), color='blue') +
+  geom_line(data=df2, aes(x=time, y=pop2), color='red') +
+  xlab("Time") +
+  ylab("Population Size") +
   theme_classic()
 
